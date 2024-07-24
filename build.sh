b63e8d8694846715e25fd7f89c3cac21d0bdccef
allegroai <none@none>
2024-07-24 03:31:27 +0300

    sync github July 2024

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker build -t dropbearbuild -f Dockerfile .
-
-docker run --rm -t -v $(pwd):/root/dropbear dropbearbuild -c "./configure --disable-utmp --disable-wtmp --disable-lastlog --disable-zlib --disable-syslog --enable-static --disable-harden && make clean && make -j4 MULTI=1 PROGRAMS=\"dropbear dropbearconvert dropbearkey\""
-
-docker image rm dropbearbuild
