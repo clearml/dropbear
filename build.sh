09bc32d7f8e6d2c3b3675f04f67aa73bb80c8369
clearrml <none@none>
2024-12-26 00:26:02 +0200

    improve build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ docker build -t dropbearbuild -f Dockerfile .
 docker run --rm -t -v $(pwd):/root/dropbear dropbearbuild -c "./configure --disable-utmp --disable-wtmp --disable-lastlog --disable-zlib --disable-syslog --enable-static --disable-harden && make clean && make -j4 MULTI=1 PROGRAMS=\"dropbear dropbearconvert dropbearkey\" && mv dropbearmulti dropbearmulti_ && make clean && mv dropbearmulti_ dropbearmulti"
 
 docker image rm dropbearbuild
+
+mkdir build
+cp dropbearmulti ./build
