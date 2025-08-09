e4afcc12a895fdb6a64a0d24b0470e442bd1a8f6
clearml <none@none>
2025-08-09 13:29:06 +0300

    fix test script to support $platform and non-root user

--- test_fixed_password_login.sh
+++ test_fixed_password_login.sh
@@ -6,7 +6,9 @@ echo "------------------------"
 PASSWORD=pa55w0rD
 echo "TEST PASSWORD FOR ANY USER $PASSWORD"
 
-docker run --rm -p 10022:10022 -v $(pwd)/build:/root/dropbear ubuntu:24.04 bash -c "/root/dropbear/dropbearmulti dropbearkey -t ed25519 -f dropbear_ed25519_host_key && mkdir -p /etc/dropbear && cp dropbear_ed25519_host_key* /etc/dropbear/ && DROPBEAR_CLEARML_FIXED_PASSWORD=$PASSWORD /root/dropbear/dropbearmulti dropbear -e -K 30 -I 0 -F -p 10022" &
+platform=${platform:-linux/amd64}
+
+docker run --rm --user 1000 -p 10022:10022 -v $(pwd)/build/$platform:/dropbear ubuntu:24.04 bash -c "/dropbear/dropbearmulti dropbearkey -t ed25519 -f /tmp/dropbear_ed25519_host_key && DROPBEAR_CLEARML_FIXED_PASSWORD=$PASSWORD /dropbear/dropbearmulti dropbear -e -K 30 -I 0 -F -p 10022 -r /tmp/dropbear_ed25519_host_key" &
 
 echo "wait for docker 5 sec..."
 
